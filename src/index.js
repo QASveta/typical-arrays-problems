d8ae09dc33dd713033c84c1af82601c950f0c26d
svetlana pazyko <svetlanapazyko@MacBook-Pro-svetlana.local>
2020-01-20 15:42:00 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if (array){
+    let n = array.length;
+    while (n){
+      return Math.min(...array);
+    }
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array){
+    let n = array.length;
+    while (n){
+      return Math.max(...array);
+    }
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array){
+    let n = array.length;
+    while (n){
+      return array.reduce((a,b) => a + b, 0) / array.length;
+    }
+  }
   return 0;
 }
